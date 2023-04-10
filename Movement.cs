c0c465b6e11ecf9e500b74090ff9463b5c2cf2fa
yuchan15 <116305517+yuchan15@users.noreply.github.com>
2023-04-10 22:34:54 +0200

    Basic movement and boy model

---
+++ Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.TransformDirection(Vector3.back) * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.TransformDirection(Vector3.right) * Time.deltaTime * speed;
+        }
+
+    }
+}
